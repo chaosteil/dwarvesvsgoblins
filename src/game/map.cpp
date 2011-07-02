f5e67360c3846d3c1b2caaf06d11d9ae6255c76f
Dominykas Djačenka <chaosteil@gmail.com>
2011-07-02 19:10:45 +0300

    Updated map to return a null tile on invalid tiles.
    * Also moved position to utils.

--- src/game/map.cpp
+++ src/game/map.cpp
@@ -6,12 +6,13 @@ namespace dvg {
 namespace game {
 
 Map::Map(int width, int height)
-  : width_(width), height_(height), data_(new Tile*[width*height]) {
+  : width_(width), height_(height), null_(0),
+    data_(new Tile*[width*height]) {
 
-  // Generate random tiles [0, 1]
+  // Generate random tiles [1, 2]
   int size = width_ * height_;
   for (int i = 0; i < size; i++) {
-    data_[i] = new Tile(std::rand() % 2);
+    data_[i] = new Tile(std::rand() % 2 + 1);
   }
 }
 
--- src/game/map.h
+++ src/game/map.h
@@ -15,15 +15,37 @@ class Map {
   int height() const { return height_; }
 
   void set_data(int x, int y, char data) {
+    if (!OnMap(x, y)) {
+      return;
+    }
+
     data_[TranslateTile(x, y)]->set_data(data);
   }
-  const Tile &data(int x, int y) const { return *data_[TranslateTile(x, y)]; }
+  const Tile &data(int x, int y) const {
+    if (!OnMap(x, y)) {
+      return null_;
+    }
+
+    return *data_[TranslateTile(x, y)]; }
 
  private:
-  int TranslateTile(int x, int y) const { return x + y * width_; }
+  int TranslateTile(int x, int y) const {
+    return x + y * width_;
+  }
+
+  bool OnMap(int x, int y) const {
+    if (x < 0 || x >= width_ || y < 0 || y >= height_) {
+      return false;
+    }
+
+    return true;
+  }
 
   int width_;
   int height_;
+
+  Tile null_;
+
   Tile **data_;
 };
 
