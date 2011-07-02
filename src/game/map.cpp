3fa81d80daa105694b852685eec57148880cd0c2
Dominykas Djačenka <chaosteil@gmail.com>
2011-07-02 17:22:01 +0300

    Fleshed out map and tile class a bit.

--- src/game/map.cpp
+++ src/game/map.cpp
@@ -1,7 +1,6 @@
 #include "game/map.h"
 
 #include <cstdlib>
-#include "game/tile.h"
 
 namespace dvg {
 namespace game {
@@ -10,10 +9,9 @@ Map::Map(int width, int height)
   : width_(width), height_(height), data_(new Tile*[width*height]) {
 
   // Generate random tiles [0, 1]
-  for (int y = 0; y < height_; y++) {
-    for (int x = 0; x < width_; x++) {
-      data_[x + y * width_] = new Tile(std::rand() % 2);
-    }
+  int size = width_ * height_;
+  for (int i = 0; i < size; i++) {
+    data_[i] = new Tile(std::rand() % 2);
   }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include "dvg_config.h"
 
-int main(int , const char **) {
+int main(int, const char **) {
   std::cout << "Dwarves vs. Goblins\nVersion: "
             << DVG_VERSION_STRING << std::endl;
   return 0;
