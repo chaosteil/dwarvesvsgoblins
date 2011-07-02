c3dc988e384206f5924f2a7e1f447662d4494c67
Dominykas Djačenka <chaosteil@gmail.com>
2011-07-02 20:56:54 +0300

    Restructured QuadTree a bit.

---
+++ src/utils/quadtree.cpp
@@ -0,0 +1,79 @@
+#ifndef DVG_SRC_UTILS_QUADTREE_CPP_
+#define DVG_SRC_UTILS_QUADTREE_CPP_
+
+#include "utils/quadtree.h"
+
+namespace dvg {
+namespace utils {
+
+template<class Item>
+QuadTreeNode<Item>::QuadTreeNode(const Vector2d &topleft, const Vector2d &size,
+                                 QuadTreeNode *parent, int level, int max_items)
+  : topleft_(topleft), size_(size), bottomright_(topleft_+size_),
+    center_(bottomright_.x()-size_.x()/2, bottomright_.y()-size_.y()/2),
+    parent_(parent), nodes_(NULL),
+    level_(level), max_items_(max_items), item_count_(0) {}
+
+template<class Item>
+QuadTreeNode<Item>::~QuadTreeNode() {
+  if (nodes_ != NULL) {
+    delete nodes_;
+  }
+}
+
+template<class Item>
+void QuadTreeNode<Item>::Insert(Item item, const Vector2d &pos) {
+  if (level_ > 0 && items_.size() >= max_items_) {
+    Split();
+    int quadrant = Quadrant(pos);
+    nodes_->at(quadrant).Insert(item, pos);
+  } else {
+    items_.push_back(item);
+  }
+
+  item_count_++;
+}
+
+template<class Item>
+int QuadTreeNode<Item>::Quadrant(const Vector2d &pos) {
+  if (pos.x() < topleft_.x() || pos.x() >= bottomright_.x() ||
+      pos.y() < topleft_.y() || pos.y() >= bottomright_.y()) {
+    return -1;
+  }
+
+  if (pos.x() < center_.x()) {
+    if (pos.y() < center_.y()) {
+      return 0;  // Top Left
+    } else {
+      return 3;  // Bottom Left
+    }
+  } else {
+    if (pos.y() < center_.y()) {
+      return 1;  // Top Right
+    } else {
+      return 2;  // Bottom Right
+    }
+  }
+}
+
+template<class Item>
+void QuadTreeNode<Item>::Split() {
+  if (!nodes_) {
+    nodes_ = new std::vector<QuadTreeNode>();
+    Vector2d size(size_.x()/2, size_.y()/2);
+
+    for (int i = 0; i < 4; i++) {
+      Vector2d topleft(topleft_.x() + size.x() * (i%2),
+                       topleft_.y() + size.y() * (i/2));
+
+      QuadTreeNode node(topleft, size, this, level_-1, max_items_);
+
+      nodes_.push_back(node);
+    }
+  }
+}
+
+}  // namespace utils
+}  // namespace dvg
+
+#endif  // DVG_SRC_UTILS_QUADTREE_CPP_
--- src/utils/quadtree.h
+++ src/utils/quadtree.h
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <vector>
+#include "utils/vector2d.h"
 
 namespace dvg {
 namespace utils {
@@ -11,78 +12,26 @@ template<class Item>
 class QuadTreeNode {
  public:
   QuadTreeNode(const Vector2d &topleft, const Vector2d &size,
-               QuadTreeNode *parent, int level, int max_items)
-      : topleft_(topleft), size_(size),
-        downright_(topleft_+size_),
-        center_(downright_.x()-size_.x()/2, downright_.y()-size_.y()/2),
-        parent_(parent), nodes_(NULL),
-        level_(level), max_items_(max_items), item_count_(0) {}
-
-  virtual ~QuadTreeNode() {
-    if (nodes_ != NULL) {
-      delete nodes_;
-    }
-  }
+               QuadTreeNode *parent, int level, int max_items);
+  virtual ~QuadTreeNode();
 
   int item_count() const { return item_count_; }
 
   const std::list<Item> &items() const { return items_; }
+  const std::vector<QuadTreeNode> &nodes() const { return nodes_; }
 
-  void Insert(Item item, const Vector2d &pos) {
-    if (level_ > 0 && items_.size() >= max_items_) {
-      Split();
-      int quadrant = Quadrant(pos);
-      nodes_->at(quadrant).Insert(item, pos);
-    } else {
-      items_.push_back(item);
-    }
-
-    item_count_++;
-  }
+  void Insert(Item item, const Vector2d &pos);
 
  private:
-  int Quadrant(const Vector2d &pos) {
-    if (pos.x() < topleft_.x() || pos.x() >= downright_.x() ||
-        pos.y() < topleft_.y() || pos.y() >= downright_.y()) {
-      return -1;
-    }
+  int Quadrant(const Vector2d &pos);
 
-    if (pos.x() < center_.x()) {
-      if (pos.y() < center_.y()) {
-        return 0;
-      } else {
-        return 3;
-      }
-    } else {
-      if (pos.y() < center_.y()) {
-        return 1;
-      } else {
-        return 2;
-      }
-    }
-  }
-
-  void Split() {
-    if (!nodes_) {
-      nodes_ = new std::vector<QuadTreeNode>();
-      Vector2d size(size_.x()/2, size_.y()/2);
-
-      for (int i = 0; i < 4; i++) {
-        Vector2d topleft(topleft_.x() + size.x() * (i%2),
-                         topleft_.y() + size.y() * (i/2));
-
-        QuadTreeNode node(topleft, size, this, level_-1, max_items_);
-
-        nodes_.push_back(node);
-      }
-    }
-  }
+  void Split();
 
   std::list<Item> items_;
 
   Vector2d topleft_;
   Vector2d size_;
-  Vector2d downright_;
+  Vector2d bottomright_;
   Vector2d center_;
 
   QuadTreeNode *parent_;
@@ -105,3 +54,5 @@ class QuadTree : public QuadTreeNode<Item> {
 }  // namespace dvg
 
 #endif  // DVG_SRC_UTILS_QUADTREE_H_
+
+#include "utils/quadtree.cpp"
