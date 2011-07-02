d39e0f5b6ffa800bc6c3e27185d9fb9ca2710413
Dominykas Djačenka <chaosteil@gmail.com>
2011-07-02 14:35:15 +0300

    Added simple compilation system via CMake.

---
+++ src/dvg_config.h
@@ -0,0 +1,11 @@
+#ifndef DVG_SRC_DVG_CONFIG_IN_
+#define DVG_SRC_DVG_CONFIG_IN_
+
+// Dwarves vs Goblins version strings
+#define DVG_VERSION_MAJOR 0
+#define DVG_VERSION_MINOR 1
+#define DVG_VERSION_STATUS "pre"
+#define DVG_VERSION_STRING \
+    "0.1 pre"
+
+#endif  // DVG_SRC_DVG_CONFIG_IN_
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * File: src/main.cpp
+ */
+
+#include <iostream>
+#include "dvg_config.h"
+
+int main(int , const char **) {
+  std::cout << "Dwarves vs. Goblins\nVersion: "
+            << DVG_VERSION_STRING << std::endl;
+  return 0;
+}
