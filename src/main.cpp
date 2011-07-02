2a33e8e09be458efc9b6e91bb1022b5ab3dc865e
ZadrraS <zadrras@gmail.com>
2011-07-02 18:45:35 +0300

    Implemented a test controllable camera (view).

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,38 @@ int main(int, const char **) {
   std::cout << "Dwarves vs. Goblins\nVersion: "
             << DVG_VERSION_STRING << std::endl;
 
-  sf::RenderWindow screen(sf::VideoMode(800, 600, 32), "Dwarves vs. Goblins");
+  sf::VideoMode video_mode(800, 600, 32);
+  sf::RenderWindow screen(video_mode, "Dwarves vs. Goblins");
+  sf::View view(sf::Vector2f(video_mode.Width / 2, video_mode.Height / 2),
+                sf::Vector2f(video_mode.Width / 2, video_mode.Height / 2));
+                
+  screen.SetView(view);
+
   dvg::game::Map test_map(20, 20);
   dvg::graphics::MapRenderer test_map_renderer(test_map);
   
   sf::Event event;
   bool running = true;
   while (running) {
+      screen.Clear(sf::Color(0, 0, 0));
       test_map_renderer.Render(screen);
       screen.Display();
       
       while (screen.GetEvent(event)) {
         if (event.Type == sf::Event::Closed) {
           running = false;
-        } else if (event.Type == sf::Event::KeyPressed &&
-                 event.Key.Code == sf::Key::Escape) {
-          running = false;
+        } else if (event.Type == sf::Event::KeyPressed) {
+          if (event.Key.Code == sf::Key::Escape) {
+            running = false;
+          } else if (event.Key.Code == sf::Key::A) {
+            view.Move(-5, 0);
+          } else if (event.Key.Code == sf::Key::D) {
+            view.Move(5, 0);
+          } else if (event.Key.Code == sf::Key::W) {
+            view.Move(0, -5);
+          } else if (event.Key.Code == sf::Key::S) {
+            view.Move(0, 5);
+          }
         }
       }
   }
