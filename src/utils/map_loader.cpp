2cb08a5fed680e2cf88ec44b5afaab0d24144e2b
crashed <mishkinis@gmail.com>
2011-07-16 22:04:12 +0300

    Changes to map loader.
    Updated the map loader so it could read the new map config structure.
    Balys is not here anymore, i'm sad.

--- src/utils/map_loader.cpp
+++ src/utils/map_loader.cpp
@@ -2,8 +2,10 @@
 
 #include <fstream> 
 #include <vector>
+#include <utility>
 
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
 
 #include "utils/map_data.h"
 
@@ -11,7 +13,7 @@ namespace dvg {
 namespace utils {
 
 const std::string MapLoader::map_extension = ".json";
-const std::string MapLoader::map_folder = "../maps/";
+const std::string MapLoader::map_folder = "../../../data/maps/";
 
 MapLoader::MapLoader(){}
 
@@ -27,40 +29,37 @@ MapData *MapLoader::Load(std::string &map_name){
 
 	map_data->SetName(root.get<std::string>("name"));
 	map_data->SetType(root.get<std::string>("type"));
+	map_data->SetTileSet(root.get<std::string>("tile_set"));
 	map_data->SetWidth(root.get<int>("width"));
-	map_data->SetHeight(root.get<int>("height"));
-	
-	int number_of_players = root.get<int>("number_of_players");
+	map_data->SetHeight(root.get<int>("height"));	
 
-	map_data->SetNumberOfPlayers(number_of_players);
-
-	for (int i = 1; i <= number_of_players; i++){
-		std::string player("player ");
-		player += (i + 48);
-		const boost::property_tree::ptree &array = root.get_child(player);
-		boost::property_tree::ptree::const_iterator it = array.begin();
-		int pos1 = it->second.get_value<int>();
-		it++;
-		int pos2 = it->second.get_value<int>();
-		map_data->AddStartPosition((double)pos1, (double)pos2);
-	}
-	
 	int width = map_data->GetWidth();
 	int height = map_data->GetHeight();
 
-	const boost::property_tree::ptree &data = root.get_child("tiles");
-	boost::property_tree::ptree::const_iterator it = data.begin();
-
-	std::vector<int> tiles;
-
-	for (int x = 0; x < width; x++){
-		for (int y = 0; y < height; y++){
-			tiles.push_back(it->second.get_value<int>());
-			it++;
+	{
+		const boost::property_tree::ptree &player_data = root.get_child("players");
+		typedef std::pair<const std::string, boost::property_tree::ptree> ptree_pair;
+		
+		BOOST_FOREACH(const ptree_pair &player, player_data){
+			map_data->AddStartPosition((double)player.second.get<int>("x"), (double)player.second.get<int>("y"));
 		}
 	}
 
-	map_data->SetTiles(tiles);
+	{
+		const boost::property_tree::ptree &tile_data = root.get_child("tiles");
+		boost::property_tree::ptree::const_iterator it = tile_data.begin();
+
+		std::vector<int> tiles;
+
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < height; y++){
+				tiles.push_back(it->second.get_value<int>());
+				it++;
+			}
+		}
+
+		map_data->SetTiles(tiles);
+	}
 
 	return map_data;
 }
